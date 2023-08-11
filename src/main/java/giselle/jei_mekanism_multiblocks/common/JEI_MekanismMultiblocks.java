0e798413b7a0ec2e8d3f190c321d15815ae0ed68
gisellevonbingen <gisellevonbingen@gmail.com>
2023-08-11 21:13:06 +0900

    Add config

--- src/main/java/giselle/jei_mekanism_multiblocks/common/JEI_MekanismMultiblocks.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/common/JEI_MekanismMultiblocks.java
@@ -4,12 +4,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import giselle.jei_mekanism_multiblocks.client.JEI_MekanismMultiblocks_Client;
+import giselle.jei_mekanism_multiblocks.common.config.JEI_MekanismMultiblocks_Config;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -24,6 +26,7 @@ public class JEI_MekanismMultiblocks
 
 	public JEI_MekanismMultiblocks()
 	{
+		JEI_MekanismMultiblocks_Config.registerConfigs(ModLoadingContext.get());
 		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> JEI_MekanismMultiblocks_Client::init);
 
 		IEventBus fml_bus = FMLJavaModLoadingContext.get().getModEventBus();
