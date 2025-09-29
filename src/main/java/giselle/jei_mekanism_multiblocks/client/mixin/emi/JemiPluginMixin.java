229ddb42e2e1a244b49755f6512b42449811eb53
Giselle <gisellevonbingen@gmail.com>
2025-09-29 22:41:21 +0900

    Fixed "Recipe null loaded with unregistered category"

--- src/main/java/giselle/jei_mekanism_multiblocks/client/mixin/emi/JemiPluginMixin.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/mixin/emi/JemiPluginMixin.java
@@ -1,22 +1,31 @@
 package giselle.jei_mekanism_multiblocks.client.mixin.emi;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import dev.emi.emi.api.EmiRegistry;
-import dev.emi.emi.jemi.JemiCategory;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.jemi.JemiPlugin;
 import dev.emi.emi.jemi.JemiRecipe;
 import giselle.jei_mekanism_multiblocks.client.emi.EMIMultiblockRecipe;
 import giselle.jei_mekanism_multiblocks.client.jei.JeiPlugin;
 import giselle.jei_mekanism_multiblocks.client.jei.MultiblockCategory;
 import giselle.jei_mekanism_multiblocks.client.jei.MultiblockWidget;
+import mezz.jei.api.recipe.category.IRecipeCategory;
 
 @Mixin(value = JemiPlugin.class, remap = false)
 public abstract class JemiPluginMixin
 {
+	@Shadow(remap = false)
+	private static Map<EmiRecipeCategory, IRecipeCategory<?>> CATEGORY_MAP;
+
 	@Inject(method = "register", remap = false, at = @At(value = "TAIL"))
 	private void register(EmiRegistry registry, CallbackInfo ci)
 	{
@@ -25,17 +34,23 @@ public abstract class JemiPluginMixin
 
 	private void jei_mekanism_multiblocks$register(EmiRegistry registry)
 	{
+		Map<IRecipeCategory<?>, EmiRecipeCategory> reverse = new HashMap<>();
+
+		for (Entry<EmiRecipeCategory, IRecipeCategory<?>> entry : CATEGORY_MAP.entrySet())
+		{
+			reverse.put(entry.getValue(), entry.getKey());
+		}
+
 		for (MultiblockCategory<? extends MultiblockWidget> jeiCategory : JeiPlugin.instance().getCategories())
 		{
-			registry.addRecipe(this.createRecipe(jeiCategory));
+			registry.addRecipe(this.createRecipe(jeiCategory, reverse.get(jeiCategory)));
 		}
 
 	}
 
-	private <WIDGET extends MultiblockWidget> EMIMultiblockRecipe<WIDGET> createRecipe(MultiblockCategory<WIDGET> jeiCategory)
+	private <WIDGET extends MultiblockWidget> EMIMultiblockRecipe<WIDGET> createRecipe(MultiblockCategory<WIDGET> jeiCategory, EmiRecipeCategory emiCategory)
 	{
 		WIDGET jeiRecipe = JeiPlugin.instance().createWidget(jeiCategory);
-		JemiCategory emiCategory = new JemiCategory(jeiCategory);
 		EMIMultiblockRecipe<WIDGET> emiRecipe = new EMIMultiblockRecipe<>(new JemiRecipe<>(emiCategory, jeiCategory, jeiRecipe));
 		jeiRecipe.addChangedHandler(w -> emiRecipe.invalidate());
 		return emiRecipe;
