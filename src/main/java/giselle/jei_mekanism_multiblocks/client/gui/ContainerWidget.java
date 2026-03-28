bd01fed7fe20ae3f02319d57bed983d57ee088d2
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-03-29 00:23:53 +0900

    Fission Layout

--- src/main/java/giselle/jei_mekanism_multiblocks/client/gui/ContainerWidget.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/gui/ContainerWidget.java
@@ -36,6 +36,23 @@ public class ContainerWidget extends Widget
 		return this.children.contains(widget);
 	}
 
+	public Widget getChildUnderMouse(double pMouseX, double pMouseY)
+	{
+		for (Widget widget : this.getChildren())
+		{
+			double childMouseX = this.toChildX(pMouseX);
+			double childMouseY = this.toChildY(pMouseY);
+
+			if (widget.isMouseOver(childMouseX, childMouseY))
+			{
+				return widget;
+			}
+
+		}
+
+		return null;
+	}
+
 	public List<Widget> getChildren()
 	{
 		return this.unmodifiableChildren;
