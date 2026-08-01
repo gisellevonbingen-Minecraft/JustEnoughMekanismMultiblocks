ee22baf0a0ab75ad6608407fa78dd6e7de3361b2
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-08-01 11:51:48 +0900

    checkTempUnitChanged

--- src/main/java/giselle/jei_mekanism_multiblocks/client/jei/MultiblockWidget.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/jei/MultiblockWidget.java
@@ -18,6 +18,8 @@ import giselle.jei_mekanism_multiblocks.client.gui.ListWidget;
 import giselle.jei_mekanism_multiblocks.client.gui.TabButtonWidget;
 import giselle.jei_mekanism_multiblocks.client.gui.TextAlignment;
 import giselle.jei_mekanism_multiblocks.client.jei.category.ICostConsumer;
+import mekanism.common.config.MekanismConfig;
+import mekanism.common.util.UnitDisplayUtils.TemperatureUnit;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.components.AbstractWidget;
@@ -43,6 +45,7 @@ public abstract class MultiblockWidget extends ContainerWidget
 	protected IntSliderWithButtons heightWidget;
 	protected CheckBoxWidget useGlassCheckBox;
 
+	private TemperatureUnit lastTempUnit;
 	private boolean needNotifyStateChange;
 
 	public MultiblockWidget()
@@ -277,6 +280,8 @@ public abstract class MultiblockWidget extends ContainerWidget
 
 		super.render(pPoseStack, pMouseX, pMouseY, pPartialTicks);
 
+		this.checkTempUnitChanged();
+
 		if (this.needNotifyStateChange)
 		{
 			this.needNotifyStateChange = false;
@@ -285,6 +290,18 @@ public abstract class MultiblockWidget extends ContainerWidget
 			this.notifyStateChange();
 
 			this.changedHandlers.forEach(h -> h.accept(this));
+		}
+
+	}
+
+	private void checkTempUnitChanged()
+	{
+		TemperatureUnit tempUnit = MekanismConfig.common.tempUnit.get();
+
+		if (this.lastTempUnit == null || lastTempUnit != tempUnit)
+		{
+			this.lastTempUnit = tempUnit;
+			this.markNeedUpdate();
 		}
 
 	}
