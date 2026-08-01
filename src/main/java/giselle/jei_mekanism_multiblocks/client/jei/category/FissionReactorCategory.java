52e2e3d4907dd30bcfceabdc4f3a2b9eca754445
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-08-01 13:09:05 +0900

    onLayoutChanged on ctor

--- src/main/java/giselle/jei_mekanism_multiblocks/client/jei/category/FissionReactorCategory.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/jei/category/FissionReactorCategory.java
@@ -85,6 +85,7 @@ public class FissionReactorCategory extends MultiblockCategory<FissionReactorCat
 			this.currentLayout = this.createEmptyLayout();
 			this.advancedLayout = this.createEmptyLayout();
 			this.advancedLayout.resetPillars();
+			this.onLayoutChanged();
 		}
 
 		public Layout createEmptyLayout()
