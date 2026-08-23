e48b2c87ea11058973f56a85270b6bd9122b521b
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-08-23 22:56:02 +0900

    Update EvaporationPlantCategory.java

--- src/main/java/giselle/jei_mekanism_multiblocks/client/jei/category/EvaporationPlantCategory.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/jei/category/EvaporationPlantCategory.java
@@ -225,9 +225,9 @@ public class EvaporationPlantCategory extends MultiblockCategory<EvaporationPlan
 
 		private void createRequiredHeaterEnergyWidget(Consumer<AbstractWidget> consumer)
 		{
+			FloatingLong plainRequiredEnergy = this.getRequiredHeaterEnergy(HeatAPI.AMBIENT_TEMP);
 			FloatingLong coldestRequiredEnergy = this.getRequiredHeaterEnergy(HeatAPI.getAmbientTemp(Integer.MIN_VALUE));
 			FloatingLong hotestRequiredEnergy = this.getRequiredHeaterEnergy(HeatAPI.getAmbientTemp(Integer.MAX_VALUE));
-			FloatingLong plainRequiredEnergy = this.getRequiredHeaterEnergy(HeatAPI.AMBIENT_TEMP);
 			ResultWidget requiredEnergyWidget = new ResultWidget(new TranslatableComponent("text.jei_mekanism_multiblocks.result.required_heater_usage"), new TranslatableComponent("%s/t", EnergyDisplay.of(plainRequiredEnergy).getTextComponent()));
 			requiredEnergyWidget.setTooltip(//
 					new TranslatableComponent("text.jei_mekanism_multiblocks.tooltip.required_heater_usage.plain", new TranslatableComponent("%s %s/t", TextUtils.format(plainRequiredEnergy.longValue()), new TranslatableComponent(MekanismLang.ENERGY_JOULES_SHORT.getTranslationKey()))), //
