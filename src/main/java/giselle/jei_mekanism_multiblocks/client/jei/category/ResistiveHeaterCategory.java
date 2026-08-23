6fbbca23c65731c38dc4ecb529a8b0a58083f464
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-08-23 22:30:31 +0900

    plain, coldest, hottest

--- src/main/java/giselle/jei_mekanism_multiblocks/client/jei/category/ResistiveHeaterCategory.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/jei/category/ResistiveHeaterCategory.java
@@ -7,25 +7,24 @@ import net.minecraft.core.Direction;
 
 public class ResistiveHeaterCategory
 {
-	public static double getStableTemp(double heat)
+	public static double getStableTemp(double ambientTemp, double heat, double inverseConduction)
 	{
 		double tempTarget = heat / TileEntityResistiveHeater.HEAT_CAPACITY;
-		double inverseConduction = HeatAPI.DEFAULT_INVERSE_CONDUCTION + TileEntityResistiveHeater.INVERSE_CONDUCTION_COEFFICIENT;
-
-		return (tempTarget * inverseConduction) + HeatAPI.AMBIENT_TEMP;
+		double totalInvConduction = inverseConduction + TileEntityResistiveHeater.INVERSE_CONDUCTION_COEFFICIENT;
+		return (tempTarget * totalInvConduction) + ambientTemp;
 	}
 
-	public static double getEnvironmentLossHeat(double temp)
+	public static double getEnvironmentLossHeat(double ambientTemp, double heaterTemp)
 	{
-		double invConduction = HeatAPI.AIR_INVERSE_COEFFICIENT + TileEntityResistiveHeater.INVERSE_INSULATION_COEFFICIENT + TileEntityResistiveHeater.INVERSE_CONDUCTION_COEFFICIENT;
-		double tempToTransfer = (temp - HeatAPI.AMBIENT_TEMP) / invConduction;
+		double totalInvConduction = HeatAPI.AIR_INVERSE_COEFFICIENT + TileEntityResistiveHeater.INVERSE_INSULATION_COEFFICIENT + TileEntityResistiveHeater.INVERSE_CONDUCTION_COEFFICIENT;
+		double tempToTransfer = (heaterTemp - ambientTemp) / totalInvConduction;
 		return Direction.values().length * tempToTransfer * TileEntityResistiveHeater.HEAT_CAPACITY;
 	}
 
-	public static double getHeatTransferableEnergy(double heat)
+	public static double getHeatTransferableEnergy(double ambientTemp, double heat, double inverseConduction)
 	{
-		double stableTemp = ResistiveHeaterCategory.getStableTemp(heat);
-		double envLossHeat = ResistiveHeaterCategory.getEnvironmentLossHeat(stableTemp);
+		double stableTemp = ResistiveHeaterCategory.getStableTemp(ambientTemp, heat, inverseConduction);
+		double envLossHeat = ResistiveHeaterCategory.getEnvironmentLossHeat(ambientTemp, stableTemp);
 		return ResistiveHeaterCategory.getEnergyForHeat(heat + envLossHeat);
 	}
 
