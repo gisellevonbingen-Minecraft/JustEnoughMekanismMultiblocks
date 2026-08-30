50bb12f254ec0dfef545d6258d2061035d54c0a3
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-08-30 13:59:33 +0900

    Rename

--- src/main/java/giselle/jei_mekanism_multiblocks/client/JEI_MekanismMultiblocks_Client.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/JEI_MekanismMultiblocks_Client.java
@@ -10,10 +10,10 @@ public class JEI_MekanismMultiblocks_Client
 {
 	private static int SAVE_TIMER = 0;
 
-	public static boolean PRESSED;
-	public static boolean RELEASED;
-	public static boolean DRAGGED;
-	public static double SCROLLED;
+	public static boolean MOUSE_PRESSED;
+	public static boolean MOUSE_RELEASED;
+	public static boolean MOUSE_DRAGGED;
+	public static double MOUSE_SCROLLED;
 
 	public static void init()
 	{
@@ -47,10 +47,10 @@ public class JEI_MekanismMultiblocks_Client
 	@SubscribeEvent
 	public static void onScreenRenderPre(ScreenEvent.Render.Post e)
 	{
-		PRESSED = false;
-		RELEASED = false;
-		DRAGGED = false;
-		SCROLLED = 0.0D;
+		MOUSE_PRESSED = false;
+		MOUSE_RELEASED = false;
+		MOUSE_DRAGGED = false;
+		MOUSE_SCROLLED = 0.0D;
 	}
 
 	@SubscribeEvent
@@ -58,7 +58,7 @@ public class JEI_MekanismMultiblocks_Client
 	{
 		if (e.getButton() == 0)
 		{
-			PRESSED = true;
+			MOUSE_PRESSED = true;
 		}
 
 	}
@@ -68,7 +68,7 @@ public class JEI_MekanismMultiblocks_Client
 	{
 		if (e.getButton() == 0)
 		{
-			RELEASED = true;
+			MOUSE_RELEASED = true;
 		}
 
 	}
@@ -78,7 +78,7 @@ public class JEI_MekanismMultiblocks_Client
 	{
 		if (e.getMouseButton() == 0)
 		{
-			DRAGGED = true;
+			MOUSE_DRAGGED = true;
 		}
 
 	}
@@ -86,7 +86,7 @@ public class JEI_MekanismMultiblocks_Client
 	@SubscribeEvent
 	public static void onMouseScrolled(ScreenEvent.MouseScrolled.Pre e)
 	{
-		SCROLLED = e.getScrollDeltaY();
+		MOUSE_SCROLLED = e.getScrollDeltaY();
 	}
 
 }
