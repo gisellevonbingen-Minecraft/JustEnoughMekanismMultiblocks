4d87652fe2d241a735bd474913a359068d96933d
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2026-09-05 11:02:41 +0900

    Update DurationTextHelper.java

--- src/main/java/giselle/jei_mekanism_multiblocks/common/util/DurationTextHelper.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/common/util/DurationTextHelper.java
@@ -3,7 +3,6 @@ package giselle.jei_mekanism_multiblocks.common.util;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 import mekanism.common.util.text.TextUtils;
 import net.minecraft.network.chat.Component;
@@ -20,15 +19,32 @@ public class DurationTextHelper
 
 	public static Component duration(long ticks)
 	{
-		List<Component> components = new ArrayList<>();
-		Consumer<Component> add = c ->
+		if (ticks <= 0L)
 		{
+			return ticks(ticks);
+		}
+
+		List<Component> components = new ArrayList<>();
+		ComponentAccumulator add = (translationKey, value) ->
+		{
+			if (value <= 0L)
+			{
+				return;
+			}
+
+			String format = "";
+
 			if (!components.isEmpty())
 			{
 				components.add(DELIMITER);
+				format = "%02d";
+			}
+			else
+			{
+				format = "%01d";
 			}
 
-			components.add(c);
+			components.add(Component.translatable(translationKey, String.format(format, value)));
 		};
 
 		Duration duration = Duration.ofSeconds(ticks / 20);
@@ -38,30 +54,11 @@ public class DurationTextHelper
 		long seconds = duration.toSeconds() % 60;
 		ticks = ticks % 20;
 
-		if (days > 0)
-		{
-			add.accept(Component.translatable("text.jei_mekanism_multiblocks.duration.days", days));
-		}
-
-		if (hours > 0)
-		{
-			add.accept(Component.translatable("text.jei_mekanism_multiblocks.duration.hours", String.format("%02d", hours)));
-		}
-
-		if (minutes > 0)
-		{
-			add.accept(Component.translatable("text.jei_mekanism_multiblocks.duration.minutes", String.format("%02d", minutes)));
-		}
-
-		if (seconds > 0)
-		{
-			add.accept(Component.translatable("text.jei_mekanism_multiblocks.duration.seconds", String.format("%02d", seconds)));
-		}
-
-		if (ticks > 0)
-		{
-			add.accept(Component.translatable("text.jei_mekanism_multiblocks.duration.ticks", String.format("%02d", ticks)));
-		}
+		add.accept("text.jei_mekanism_multiblocks.duration.days", days);
+		add.accept("text.jei_mekanism_multiblocks.duration.hours", hours);
+		add.accept("text.jei_mekanism_multiblocks.duration.minutes", minutes);
+		add.accept("text.jei_mekanism_multiblocks.duration.seconds", seconds);
+		add.accept("text.jei_mekanism_multiblocks.duration.ticks", ticks);
 
 		return components.stream().reduce(Component.empty(), MutableComponent::append, MutableComponent::append);
 	}
@@ -69,6 +66,12 @@ public class DurationTextHelper
 	private DurationTextHelper()
 	{
 
+	}
+
+	@FunctionalInterface
+	private interface ComponentAccumulator
+	{
+		void accept(String translationKey, long value);
 	}
 
 }
