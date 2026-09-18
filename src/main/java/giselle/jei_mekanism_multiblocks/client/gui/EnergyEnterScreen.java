8363dd57f63cfbbe349c36c5a4ad78cb5699a381
Giselle <44163945+gisellevonbingen@users.noreply.github.com>
2026-09-18 15:31:55 +0900

    Update EnergyEnterScreen.java

--- src/main/java/giselle/jei_mekanism_multiblocks/client/gui/EnergyEnterScreen.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/gui/EnergyEnterScreen.java
@@ -17,7 +17,7 @@ public class EnergyEnterScreen extends Screen
 {
 	private static final Component COMPONENT_ENTER = Component.translatable("text.jei_mekanism_multiblocks.enter");
 	private static final Component COMPONENT_EXAMPLE = Component.translatable("text.jei_mekanism_multiblocks.example");
-	private static final Component COMPONENT_EXAMPLES = Component.literal("1000, 100_000, 10K, 400MFE, 1GJ");
+	private static final Component COMPONENT_EXAMPLES = Component.literal("1000, 100_000, 10.5K, 400MFE, 1.5GJ");
 
 	private final DoubleConsumer doneHandler;
 
