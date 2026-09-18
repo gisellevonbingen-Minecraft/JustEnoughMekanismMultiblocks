7430b7ac1cdd7ef38c2a28849cbc0387b4a0d8eb
Giselle <44163945+gisellevonbingen@users.noreply.github.com>
2026-09-18 17:04:41 +0900

    Update LasersCategory.java

--- src/main/java/giselle/jei_mekanism_multiblocks/client/jei/category/LasersCategory.java
+++ src/main/java/giselle/jei_mekanism_multiblocks/client/jei/category/LasersCategory.java
@@ -96,7 +96,7 @@ public class LasersCategory extends MultiblockCategory<LasersCategory.LaserWidge
 			{
 				this.setTargetEnergy(jules.doubleValue());
 			});
-			screen.setJules(FloatingLong.create(this.targetEnergy));
+			screen.setJules(FloatingLong.create(this.getTargetEnergy()));
 			Minecraft.getInstance().pushGuiLayer(screen);
 		}
 
